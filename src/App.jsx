e11839f58d7658b77962a6bcd5a0f7267b433a75
PranavRao18 <prnvraosrs@gmail.com>
2023-11-08 13:07:12 +0530

    added navbar

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Contacts from "./components/Contacts";
+import Education from "./components/Education";
+import Hero from "./components/Hero";
+import Interests from "./components/Interests";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Tech from "./components/Tech";
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 br-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <Interests />
+        <Projects />
+        <Education />
+        <Tech />
+        <Contacts />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/components/Tech.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Tech(){
+    return (
+        <div>Tech</div>
+    )
+}
+
+export default Tech;
